c021f8493d6ac43a13ed852bd7c5f151afe7dc54
kp2pml30 <kp2pml30@gmail.com>
2024-02-18 13:28:18 +0300

    add scopes and few basic constructions

--- src/runtimeTypes.ts
+++ src/runtimeTypes.ts
@@ -1,7 +1,14 @@
 import * as ast from './ast'
 
-export interface Fn {
-    (...args: Value[]): Promise<Value>
+export class Fn {}
+
+export class RuntimeFn extends Fn {
+    fn: (vals: Array<Value>) => Value | undefined
+
+    constructor(fn: (vals: Array<Value>) => Value | undefined) {
+        super()
+        this.fn = fn
+    }
 }
 
 export interface List<T> extends Array<T> {
@@ -26,4 +33,15 @@ export async function transform(prog: Array<[ast.PosInfo, ast.Expr]>): Promise<A
     }
 
     return prog.map((e: [ast.PosInfo, ast.Expr]) => [dfs(e[1]), e[0]])
+}
+
+
+export function reprType(v: Value): string {
+    return JSON.stringify(v, (k, v) => {
+        if (typeof v == 'bigint') {
+            return `int:${v.toString()}`
+        } else {
+            return v;
+        }
+    })
 }
